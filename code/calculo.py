ac08a73d11399b30e3e9ee358dec7f522b5152a2
JuanFelipe <juanfelipemarin906@gmail.com>
2026-09-17 20:16:40 -0500

    calculadora, agregar archivo

---
+++ code/calculo.py
@@ -0,0 +1,18 @@
+def suma (a,b):
+    return a + b
+
+def resta (a,b):
+    return a - b
+
+
+def multi (a,b):
+    return a * b
+
+
+resultado_suma = suma(1,2)
+resultado_resta = resta(2,3)
+resultado_multi = multi(1,9)
+
+print(f"Resultado suma {resultado_suma}")
+print(f"Resultado resta {resultado_resta}")
+print(f"Resultado multiplicación {resultado_multi}")
