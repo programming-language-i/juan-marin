fcb702b79e9ce9b5533f91659c724c3c069e3572
JuanFelipe <juanfelipemarin906@gmail.com>
2026-09-24 20:53:43 -0500

    se agrega justificante en ejrcicio 3 de la parte c

--- taller-1/parte-c/ejer-3.py
+++ taller-1/parte-c/ejer-3.py
@@ -4,6 +4,7 @@
 #**(1)** qué pasa al ejecutarlo
 #**(2)** por qué
 #**(3)** la corrección mínima.
+# R/ Se agrega if __name__ == "__main__":, porque al usar ProcessPoolExecutor es obligatorio proteger el código que crea procesos hijos con esta condición para evitar la creación infinita de procesos hijos al importar el módulo.
 
 
 from concurrent.futures import ProcessPoolExecutor
